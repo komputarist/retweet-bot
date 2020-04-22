a241d0760e6180ae032104f2631cb9e551262fcd
Alhassan Atama <komputarist@gmail.com>
2020-04-22 02:36:43 +0100

    added config file

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys from Twitter Developer Account here.
+module.exports = {
+  consumer_key: 'PaQnQYUs7CmO6JG2iU6S1wd78',
+  consumer_secret: 'XxNBWkZROJZR6IdUJc9f6RV7cP6Fl2ibm6bzzV1S4f8kjbeear',
+  access_token: '216542424-eTgG8FcuZcqLhH6KFHOrjTIR8xDStuXmGiGUivpX',
+  access_token_secret: 'IMKCvMzWo3nvIfDd3iFLe8QCxmlGReERc94eCqEculLsJ'
+}
